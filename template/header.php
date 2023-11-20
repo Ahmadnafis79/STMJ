61d50b1668d47a02025092472332b5cec2d35d42
Muhamad Fikri <72112447+mfikrikuliah11@users.noreply.github.com>
2023-11-20 08:58:20 +0800

    edit logo xampp header

--- template/header.php
+++ template/header.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <link rel="icon" type="img/png" sizes="3x15" href="../img/stmj.png" />
   <meta name="description" content="" />
   <meta name="author" content="" />
   <title>STMJ</title>
