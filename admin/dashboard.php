e197eefaba8489589cd3bd5f28c212f03bc853f8
ICAY <cayfnsky@gmail.com>
2023-11-22 14:02:14 +0800

    update dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -34,12 +34,12 @@ if (session_status() == PHP_SESSION_NONE) {
                 <a href="https://docs.google.com/spreadsheets/d/1BIi8Ip1YjWT1NIwE6F_ij0NIpbde01O97Ydr6ukWaQU/edit?usp=sharing" class="list-group-item list-group-item-action">Safety</a>
                 <a href="https://docs.google.com/spreadsheets/d/1uAvzFjjR-vfNs7p9mAFKS5ZWkCK-8hcgZMRdniipbXg/edit?usp=sharing" class="list-group-item list-group-item-action">Product Knowledge</a>
                 <a href="https://docs.google.com/spreadsheets/d/1wMsCUKfcfkHMi9KKEMVrffu6caDp-zwlknN31GjwHpQ/edit?usp=sharing" class="list-group-item list-group-item-action">Tools</a>
-                <a href="#" class="list-group-item list-group-item-action">Basic Mechine Element</a>
-                <a href="#" class="list-group-item list-group-item-action">Diesel Engine</a>
-                <a href="#" class="list-group-item list-group-item-action">Hydraulic System</a>
-                <a href="#" class="list-group-item list-group-item-action">Electrical System</a>
-                <a href="#" class="list-group-item list-group-item-action">Direct Drive System</a>
-                <a href="#" class="list-group-item list-group-item-action">Torqflow Drive System</a>
+                <a href="https://docs.google.com/spreadsheets/d/1V19-pt45OHJpGLomVVlnyNEvUzRSE9t6xLmBX7X7fGE/edit#gid=0" class="list-group-item list-group-item-action">Basic Mechine Element</a>
+                <a href="https://docs.google.com/spreadsheets/d/1uXTWdRQ5XwijxokL2Snvr7039KYAsk1nINnvXMRTuOw/edit#gid=0" class="list-group-item list-group-item-action">Diesel Engine</a>
+                <a href="https://docs.google.com/spreadsheets/d/1RuZoPUDkhyXzY9baW-TytgpnSOev9JfzRGu-mh8J6j0/edit#gid=0" class="list-group-item list-group-item-action">Hydraulic System</a>
+                <a href="https://docs.google.com/spreadsheets/d/1-ZelI0TDlBfIAYd4Y4VBwE_FiYNGj-HChsM2FtWIsUM/edit#gid=0" class="list-group-item list-group-item-action">Electrical System</a>
+                <a href="https://docs.google.com/spreadsheets/d/1dAHuHtTBzoSfe5tYfU4YWD3xe1Z_bVlJhdMkbniQRP8/edit#gid=0" class="list-group-item list-group-item-action">Direct Drive System</a>
+                <a href="https://docs.google.com/spreadsheets/d/1q-6BYDLnqwXQUfz7DKQVeel-P9phylVtPVGY01TelLQ/edit#gid=0" class="list-group-item list-group-item-action">Torqflow Drive System</a>
               </div>
           </div>
         </div>
@@ -51,15 +51,15 @@ if (session_status() == PHP_SESSION_NONE) {
         <div class="card-body">
           <div class="row no-gutters align-items-center">
               <div class="list-group">
-                <a href="#" class="list-group-item list-group-item-action">Steering & Brake System</a>
-                <a href="#" class="list-group-item list-group-item-action">Steering Wheel System</a>
-                <a href="#" class="list-group-item list-group-item-action">Brake System Wheel</a>
-                <a href="#" class="list-group-item list-group-item-action">Dinal Drive & Undercariage</a>
-                <a href="#" class="list-group-item list-group-item-action">Axle, Wheel, Suspension</a>
-                <a href="#" class="list-group-item list-group-item-action">Basic Remove Install</a>
-                <a href="#" class="list-group-item list-group-item-action">Basic Troubleshooting</a>
-                <a href="#" class="list-group-item list-group-item-action">Basic Maintenance</a>
-                <a href="#" class="list-group-item list-group-item-action">Partboot Reading</a>
+                <a href="https://docs.google.com/spreadsheets/d/1oxaP2e3gxbPzHGP5s_8o2fyEf7pgEDjNAV3Xq5hn4XI/edit#gid=0" class="list-group-item list-group-item-action">Steering & Brake System</a>
+                <a href="https://docs.google.com/spreadsheets/d/1nDmTN6D2uVD-fkNFyCqd0ddV8Z_9m4rOxdNbfnYY6h0/edit#gid=0" class="list-group-item list-group-item-action">Steering Wheel System</a>
+                <a href="https://docs.google.com/spreadsheets/d/19n55aIanj1DYq4PrLNRVWyd7laBW6-uR66LidRKv6Qo/edit?usp=drive_web&ouid=109235989770855344842" class="list-group-item list-group-item-action">Brake System Wheel</a>
+                <a href="https://docs.google.com/spreadsheets/d/1YBCp_nwlCdF1BtH0GhIuBnU5kXUE8fl4g16RM4vsquM/edit#gid=0" class="list-group-item list-group-item-action">Final Drive & Undercariage</a>
+                <a href="https://docs.google.com/spreadsheets/d/15mmomtI8sFAXz8O4sV_iX8Dm36B2sqLgaXMsgh0SKjM/edit#gid=0" class="list-group-item list-group-item-action">Axle, Wheel, Suspension</a>
+                <a href="https://docs.google.com/spreadsheets/d/1KL9GwV3xUYEDDqG9qZgRWr2EmnSfC0jP6B0q6skhcvY/edit#gid=0" class="list-group-item list-group-item-action">Basic Remove Install</a>
+                <a href="https://docs.google.com/spreadsheets/d/1MjRjW4mVwAO1Rhnczk5dZtR7axG1VioKfSDuPdAUz8o/edit#gid=0" class="list-group-item list-group-item-action">Basic Troubleshooting</a>
+                <a href="https://docs.google.com/spreadsheets/d/18ZWfpU3rQB1VlxvUgzRHD0aPTLlrJDDkmSro_qFBaIA/edit#gid=0" class="list-group-item list-group-item-action">Basic Maintenance</a>
+                <a href="https://docs.google.com/spreadsheets/d/1xFidTWzC5R-LwiPhZPdn1g_uW0hwXUIvw7WqOWs6A9k/edit#gid=0" class="list-group-item list-group-item-action">Partboot Reading</a>
               </div>
           </div>
         </div>
